8fbc1e2685518812f5bfdcbcf80d3fd24e80ca2c
kXborg <kukilp213@gmail.com>
2023-08-10 00:04:07 +0530

    working on the flyer program

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from ultralytics import YOLO
+from djitellopy import tello 
 import cv2 
 import numpy as np 
 import math
+from datetime import datetime
+
 
 # Find focus point using `get_centroid()` function.
 def get_centroid(bboxes):
@@ -37,83 +40,106 @@ def colorize(img, coords):
 
     return img
 
-# Create video capture object.
-feed = cv2.VideoCapture('videos/beach-walk.mp4')
 
-# Load YOLO model.
-model = YOLO("yolov8n.pt")
+def controls():
+    pass
 
-# set counter for frame skipping (may or may not require).
-count = 0
-while True:
-    ret, frame = feed.read()
-    # increment counter.
-    count += 1
-    if not ret:
-        print('Unable to read frames!')
-        break
-    canvas = frame.copy()
-    # Get image height width.
-    height, width = frame.shape[:2]
-    # if count%2 == 0:
-    # Filter detected persons.
-    results = model.predict(source=frame, classes=0, conf=0.5)
-    boxes = results[0].boxes
-    # print(boxes)
-    
-    centroid_coords = get_centroid(boxes)
-    # print('Centroid : ', centroid_coords)
-    
-    if centroid_coords is not None:
-        xc, yc = int(centroid_coords[0]), int(centroid_coords[1])
-        cv2.circle(canvas, (xc, yc), 4, (0,0,255), -1)
 
-        # Logic for drone control goes here.
-        # Get control region coordinates at 20% margin.
-        cr_tlc_x, cr_tlc_y = int(0.20*width), int(0.20*height)
-        cr_brc_x, cr_brc_y = int(0.80*width), int(0.80*height)
+if __name__ == '__main__':
 
-        cv2.line(canvas, (0, cr_tlc_y), (width,  cr_tlc_y), (0,255,0), 1, cv2.LINE_AA)
-        cv2.line(canvas, (0, cr_brc_y), (width,  cr_brc_y), (0,255,0), 1, cv2.LINE_AA)
-        cv2.line(canvas, (cr_tlc_x, 0), (cr_tlc_x, height), (0,255,0), 1, cv2.LINE_AA)
-        cv2.line(canvas, (cr_brc_x, 0), (cr_brc_x, height), (0,255,0), 1, cv2.LINE_AA)
+    # Init drone.
+    drone = tello.Tello()
+    drone.connect()
+    battery = drone.get_battery()
+    print('Battery Percentage : ', battery)
 
-        # Centroid moved to left region.
-        if (xc < cr_tlc_x and cr_tlc_y < yc < cr_brc_y):
-            print('Focus moved to left, sending drone left')
-            # Glow left region.
-            canvas = colorize(canvas, (0, cr_tlc_y, cr_tlc_x, cr_brc_y))
-            # send move left command.
-        elif (cr_tlc_x < xc < cr_brc_x and yc < cr_tlc_y):
-            print('Focus moved to ahead, sending drone forward')
-            # Glow forward region
-            canvas = colorize(canvas, (cr_tlc_x, 0, cr_brc_x, cr_tlc_y))
-            # send move forward command.
-        elif (xc > cr_brc_x and cr_tlc_y < yc < cr_brc_y):
-            print('Focus moved to right, sending drone right')
-            # Glow right region.
-            canvas = colorize(canvas, (cr_brc_x, cr_tlc_y, width, cr_brc_y))
-            # send move right command.
-        elif(cr_tlc_x < xc < cr_brc_x and yc > cr_brc_y):
-            print('Focus moved back, sending drone back')
-            # Glow bottom region.
-            canvas = colorize(canvas, (cr_tlc_x, cr_brc_y, cr_brc_x, height))
-            # send move back command.
-        else:
-            print('Drone centered')
-    
-    # for display purpose only, control is up.
-    for box in boxes:
-        coordinates = box.xyxy.numpy()
-        x1, y1, x2, y2 = coordinates[0]
-        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-        cv2.rectangle(canvas, (x1, y1), (x2, y2), (255, 0, 0), 2, cv2.LINE_AA)
-        # print(x1, ',', x2, ',', y1, ',', y2)
+    # Start video stream.
+    drone.streamon()
+    print('Video stream started.')
 
-    cv2.imshow("Results", cv2.resize(canvas, None, fx=0.5, fy=0.5))
-    key = cv2.waitKey(1)
-    if key == ord('q'):
-        break
+    now = datetime.now()
+    date_time = now.strftime("%m-%d-%Y-%H-%M-%S")
 
-feed.release()
-cv2.destroyAllWindows()
+    # Create video writer object.
+    out = cv2.VideoWriter(f"video-{date_time}.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 25, (960, 720))
+
+    # Create video capture object.
+    feed = cv2.VideoCapture('videos/beach-walk.mp4')
+
+    # Load YOLO model.
+    model = YOLO("yolov8n.pt")
+
+    # set counter for frame skipping (may or may not require).
+    count = 0
+    while True:
+        ret, frame = feed.read()
+        # increment counter.
+        count += 1
+        if not ret:
+            print('Unable to read frames!')
+            break
+        canvas = frame.copy()
+        # Get image height width.
+        height, width = frame.shape[:2]
+        # if count%2 == 0:
+        # Filter detected persons.
+        results = model.predict(source=frame, classes=0, conf=0.5)
+        boxes = results[0].boxes
+        # print(boxes)
+        
+        centroid_coords = get_centroid(boxes)
+        # print('Centroid : ', centroid_coords)
+        
+        if centroid_coords is not None:
+            xc, yc = int(centroid_coords[0]), int(centroid_coords[1])
+            cv2.circle(canvas, (xc, yc), 4, (0,0,255), -1)
+
+            # Logic for drone control goes here.
+            # Get control region coordinates at 20% margin.
+            cr_tlc_x, cr_tlc_y = int(0.20*width), int(0.20*height)
+            cr_brc_x, cr_brc_y = int(0.80*width), int(0.80*height)
+
+            cv2.line(canvas, (0, cr_tlc_y), (width,  cr_tlc_y), (0,255,0), 1, cv2.LINE_AA)
+            cv2.line(canvas, (0, cr_brc_y), (width,  cr_brc_y), (0,255,0), 1, cv2.LINE_AA)
+            cv2.line(canvas, (cr_tlc_x, 0), (cr_tlc_x, height), (0,255,0), 1, cv2.LINE_AA)
+            cv2.line(canvas, (cr_brc_x, 0), (cr_brc_x, height), (0,255,0), 1, cv2.LINE_AA)
+
+            # Centroid moved to left region.
+            if (xc < cr_tlc_x and cr_tlc_y < yc < cr_brc_y):
+                print('Focus moved to left, sending drone left')
+                # Glow left region.
+                canvas = colorize(canvas, (0, cr_tlc_y, cr_tlc_x, cr_brc_y))
+                # send move left command.
+            elif (cr_tlc_x < xc < cr_brc_x and yc < cr_tlc_y):
+                print('Focus moved to ahead, sending drone forward')
+                # Glow forward region
+                canvas = colorize(canvas, (cr_tlc_x, 0, cr_brc_x, cr_tlc_y))
+                # send move forward command.
+            elif (xc > cr_brc_x and cr_tlc_y < yc < cr_brc_y):
+                print('Focus moved to right, sending drone right')
+                # Glow right region.
+                canvas = colorize(canvas, (cr_brc_x, cr_tlc_y, width, cr_brc_y))
+                # send move right command.
+            elif(cr_tlc_x < xc < cr_brc_x and yc > cr_brc_y):
+                print('Focus moved back, sending drone back')
+                # Glow bottom region.
+                canvas = colorize(canvas, (cr_tlc_x, cr_brc_y, cr_brc_x, height))
+                # send move back command.
+            else:
+                print('Drone centered')
+        
+        # for display purpose only, control is up.
+        for box in boxes:
+            coordinates = box.xyxy.numpy()
+            x1, y1, x2, y2 = coordinates[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            cv2.rectangle(canvas, (x1, y1), (x2, y2), (255, 0, 0), 2, cv2.LINE_AA)
+            # print(x1, ',', x2, ',', y1, ',', y2)
+
+        cv2.imshow("Results", cv2.resize(canvas, None, fx=0.5, fy=0.5))
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+
+    feed.release()
+    cv2.destroyAllWindows()
